2023bdab2ebd35666a11d8400893da3cc5c2fbc9
HP <touilanjeh003@gmail.com>
2020-12-25 13:36:41 +0100

    lots of changes done

--- app/src/main/java/com/supcom/agritrade/Post.java
+++ app/src/main/java/com/supcom/agritrade/Post.java
@@ -1,6 +1,5 @@
 package com.supcom.agritrade;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
@@ -11,6 +10,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
+import android.app.ProgressDialog;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,24 +21,23 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 
 public class Post extends AppCompatActivity {
 
-    private static final int PICK_IMAGE_REQUEST =71 ;
+    private static final int PICK_IMAGE_REQUEST =111 ;
     private Button btnChoose, Post;
     private ImageView imageView;
 
     private Uri filePath;
     FirebaseStorage storage;
-    StorageReference storageReference;
+    StorageReference storageRef;
+    ProgressDialog pd;
 
 
 
@@ -70,12 +69,6 @@ public class Post extends AppCompatActivity {
             }
         });
 
-        Post.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                uploadImage();
-            }
-        });
 
 
 
@@ -83,6 +76,34 @@ public class Post extends AppCompatActivity {
         Post.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                    if(filePath != null) {
+                        pd.show();
+
+                        StorageReference childRef = storageRef.child("image.jpg");
+
+                        //uploading the image
+                        UploadTask uploadTask = childRef.putFile(filePath);
+
+                        uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                pd.dismiss();
+                                Toast.makeText(Post.this, "Upload successful", Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                pd.dismiss();
+                                Toast.makeText(Post.this, "Upload Failed -> " + e, Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                    else {
+                        Toast.makeText(Post.this, "Select an image", Toast.LENGTH_SHORT).show();
+                    }
+
+
                 Map<String, Object> Post = new HashMap<>();
                 Post.put("Type", Type.getText().toString());
                 Post.put("Price", Price.getText().toString());
@@ -123,6 +144,7 @@ public class Post extends AppCompatActivity {
                 && data != null && data.getData() != null )
         {
              filePath = data.getData();
+
             try {
                 Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
                 imageView.setImageBitmap(bitmap);
@@ -133,38 +155,5 @@ public class Post extends AppCompatActivity {
             }
         }
     }
-    private void uploadImage() {
 
-        if(filePath != null)
-        {
-            final ProgressDialog progressDialog = new ProgressDialog(this);
-            progressDialog.setTitle("Uploading...");
-            progressDialog.show();
-
-            StorageReference ref = storageReference.child("images/"+ UUID.randomUUID().toString());
-            ref.putFile(filePath)
-                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onSuccess(@ NonNull UploadTask.TaskSnapshot taskSnapshot) {
-                            progressDialog.dismiss();
-                            Toast.makeText(Post.this, "Uploaded", Toast.LENGTH_SHORT).show();
-                        }
-                    })
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            progressDialog.dismiss();
-                            Toast.makeText(Post.this, "Failed "+e.getMessage(), Toast.LENGTH_SHORT).show();
-                        }
-                    })
-                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
-                            double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot
-                                    .getTotalByteCount());
-                            progressDialog.setMessage("Uploaded "+(int)progress+"%");
-                        }
-                    });
-        }
-    }
 }
